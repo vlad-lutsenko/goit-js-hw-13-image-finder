7c90e7522284077477b07a0b0bd165b675e0e353
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-03 17:20:28 +0300

    hw13

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
+import fetchFunc from './tools/apiService';
+import inputForm from './tools/inputForm.hbs';
+import card from './tools/imageCard.hbs';
 import './styles.css';
+
+let page = 1;
+
+const root = document.querySelector('.root');
+const key = '15871594-e6aabc3dbb9db4d877f262370';
+
+function renderInputForm() {
+  root.insertAdjacentHTML('beforeend', inputForm());
+}
+renderInputForm();
+
+const input = document.querySelector('input');
+const form = document.querySelector('#search-form');
+
+function moreBtnCreate() {
+  if (!document.querySelector('.more')) {
+    const moreBtn = document.createElement('button');
+    moreBtn.classList.add('more');
+    root.append(moreBtn);
+    moreBtn.textContent = 'тиць';
+  }
+}
+
+function renderImages(e) {
+  const gallery = document.createElement('ul');
+  root.append(gallery);
+  gallery.classList.add('gallery');
+  e.preventDefault();
+  gallery.innerHTML = '';
+  const query = input.value;
+  fetchFunc(query, page, key).then(hits => {
+    const markup = card(hits);
+    gallery.innerHTML = markup;
+    moreBtnCreate();
+    document.querySelector('.more').addEventListener('click', renderMoreImages);
+  });
+}
+
+function renderMoreImages() {
+  page += 1;
+  const gallery = document.querySelector('.gallery');
+  const query = input.value;
+  fetchFunc(query, page, key).then(hits => {
+    const markup = card(hits);
+    gallery.insertAdjacentHTML('beforeend', markup);
+    window.scrollTo({
+      top: +window.scrollY + 600,
+      behavior: 'smooth',
+    });
+  });
+}
+
+form.addEventListener('submit', renderImages);
